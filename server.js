5f6eba7af285712317b61118b9a547951de2ecdc
uknable <uknable@hotmail.com>
2020-12-16 22:39:57 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 const app = express();
 
 //////////// GET STORYBLOK CONTENT //////////////
